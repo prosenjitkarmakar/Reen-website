c245def5ff62902fc526998d97dd9432ec1ef2ef
Prosenjit Karmakar <shriprosenjitkarmakarnoyon@gmail.com>
2019-04-22 11:14:03 +0600

    this is reen-website

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// overly full screen search box
+function openSearch() {
+  document.getElementById("myOverlay").style.display = "block";
+}
+
+function closeSearch() {
+  document.getElementById("myOverlay").style.display = "none";
+}
